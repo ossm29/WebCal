5b59ecf6f9a3d6fd5ea915a337fe300686b2e7a0
ossm29 <oussamakonate@gmail.com>
2023-04-10 23:19:07 +0200

    file changes

--- src/etudiant.php
+++ src/etudiant.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['status'] !== 'etudiant') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendrier</title>
